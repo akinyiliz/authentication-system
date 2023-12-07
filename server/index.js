116232a8c530cc53426c949e9f33cf4a4b04fe82
akinyiliz <lizzieke84@gmail.com>
2023-12-07 07:36:59 +0300

    feat: Implement user login endpoint

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const User = require("../models/users");
 const dbConnection = require("../db/connect");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
@@ -49,6 +50,44 @@ app.post("/users/register", async (req, res) => {
   } catch (error) {
     console.error("Error registering a user.", error);
     return res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const user = req.body;
+
+  const secretKey = process.env.JWT_SECRET;
+
+  try {
+    const userExists = await User.findOne({
+      $or: [{ username: user.username }, { email: user.email }],
+    });
+
+    if (!userExists) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials." });
+    }
+
+    const passwordMatch = await bcrypt.compare(
+      user.password,
+      userExists.password
+    );
+
+    if (passwordMatch) {
+      const token = jwt.sign({ userId: userExists._id }, secretKey);
+      console.log(token);
+      res.json({
+        success: true,
+        message: "User login successfull.",
+        token: token,
+      });
+    } else {
+      res.status(401).json({ success: false, message: "Incorrect password." });
+    }
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    res.status(500).json({ success: false, message: error.message });
   }
 });
 
