c589adc4fb71aec8c097dd636ca11d25144a0b48
akinyiliz <lizzieke84@gmail.com>
2023-12-08 05:59:09 +0300

    refactor: Move endpoint logic to controllers folder

--- server/index.js
+++ server/index.js
@@ -1,95 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/users");
 const dbConnection = require("../db/connect");
-const jwt = require("jsonwebtoken");
+const userController = require("../controllers/UserController");
 
 const app = express();
 app.use(express.json());
 dbConnection();
 
 // ENDPOINT - REGISTER
-app.post("/users/register", async (req, res) => {
-  const user = req.body;
+app.post("/users/register", userController.register);
 
-  try {
-    const checkEmailExists = await User.findOne({ email: user.email });
-    if (checkEmailExists) {
-      return res.status(409).json({
-        success: false,
-        message: "User with this email already exists.",
-      });
-    }
+// ENDPOINT - LOGIN
+app.post("/users/login", userController.login);
 
-    const checkUsernameExists = await User.findOne({ username: user.username });
-    if (checkUsernameExists) {
-      return res.status(409).json({
-        success: false,
-        message: "User with this username already exists.",
-      });
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(user.password, salt);
-
-    user.password = hashedPassword;
-
-    const newUser = await User.create(user);
-
-    if (newUser) {
-      return res.status(201).json({
-        success: true,
-        message: "User Registration Successfull.",
-      });
-    } else {
-      return res
-        .status(400)
-        .json({ success: false, message: "User Registration Failed." });
-    }
-  } catch (error) {
-    console.error("Error registering a user.", error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-});
-
-app.post("/users/login", async (req, res) => {
-  const user = req.body;
-
-  const secretKey = process.env.JWT_SECRET;
-
-  try {
-    const userExists = await User.findOne({
-      $or: [{ username: user.username }, { email: user.email }],
-    });
-
-    if (!userExists) {
-      return res
-        .status(401)
-        .json({ success: false, message: "Invalid credentials." });
-    }
-
-    const passwordMatch = await bcrypt.compare(
-      user.password,
-      userExists.password
-    );
-
-    if (passwordMatch) {
-      const token = jwt.sign({ userId: userExists._id }, secretKey);
-      console.log(token);
-      res.json({
-        success: true,
-        message: "User login successfull.",
-        token: token,
-      });
-    } else {
-      res.status(401).json({ success: false, message: "Incorrect password." });
-    }
-  } catch (error) {
-    console.error("Error logging in user:", error);
-    res.status(500).json({ success: false, message: error.message });
-  }
-});
+// ENDPOINT - GET ALL USERS
+app.get("/users", userController.getUsers);
 
 // SERVER
 const port = process.env.PORT;
