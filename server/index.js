9b08ba5d2eca31569ebcbdd8218e440d466ade20
akinyiliz <lizzieke84@gmail.com>
2023-12-11 06:48:28 +0300

    refactor: Update main route entry point

--- server/index.js
+++ server/index.js
@@ -2,14 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const dbConnection = require("../db/connect");
 const userRoutes = require("../routes/UserRoutes");
-const userController = require("../controllers/UserController");
 
 const app = express();
 app.use(express.json());
 dbConnection();
 
 // ENDPOINT - REGISTER
-app.use("/api/users", userRoutes);
+app.use("/users", userRoutes);
 
 // SERVER
 const port = process.env.PORT;
