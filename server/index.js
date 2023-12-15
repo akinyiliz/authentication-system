01f701e52b0d7bf4563cf8e00d3d5fa4268393ea
akinyiliz <lizzieke84@gmail.com>
2023-12-15 12:56:05 +0300

    fix: Update UserRoutes import path

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const dbConnection = require("../db/connect");
-const UserRoutes = require("../routes/user.route");
+const UserRoutes = require("../routes/user.routes");
 
 const app = express();
 app.use(express.json());
